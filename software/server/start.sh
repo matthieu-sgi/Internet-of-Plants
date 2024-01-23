3ea120d36ee9a8898b521c695b7809ad655b9f5d
sn00wden <matthieu2603@gmail.com>
2024-01-23 15:37:16 +0100

    Maintenance

--- software/server/start.sh
+++ software/server/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pd -open <path>/pd_patches/receiveTCP.pd -nogui -r 44100 -alsa  -noadc  -audiooutdev 1 -outchannels 2 -listdev -stderr &
+pd -open /mnt/data/Internet_of_plants/software/server/pd_patches/receiveTCP.pd -nogui -r 44100 -alsa  -noadc  -audiooutdev 1 -outchannels 2 -listdev -stderr &
 
-python3 <path>/main.py
+python3 /mnt/data/Internet_of_plants/software/server/main.py
