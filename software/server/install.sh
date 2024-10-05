3d8b30c3042fd44d13e59d7e593b490532025140
Matthieu <matthieu2603@gmail.com>
2024-10-05 11:49:11 +0200

    Install script updating templating files

--- software/server/install.sh
+++ software/server/install.sh
@@ -19,21 +19,21 @@ pip3 install -r requirements.txt
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 # Update the start script with the current script directory
-sed -i "s|<path>|$SCRIPT_DIR|" "$SCRIPT_DIR/start.sh"
+sed "s|<path>|$SCRIPT_DIR|" "$SCRIPT_DIR/start.sh.tmpl" > start.sh 
 
 # Define the systemd service file path
+SERVICE_FILE_TEMPLATE="$SCRIPT_DIR/iop_server.service.tmpl"
 SERVICE_FILE="$SCRIPT_DIR/iop_server.service"
-
 # Check if the service file exists
-if [ -f "$SERVICE_FILE" ]; then
+if [ -f "$SERVICE_FILE_TEMPLATE" ]; then
     # Backup the original service file (if it exists)
     [ -f "/etc/systemd/system/iop_server.service" ] && cp "/etc/systemd/system/iop_server.service" "/etc/systemd/system/iop_server.service.bak"
 
     # Update the ExecStart line in the service file with the current script directory
-    sed -i "s|ExecStart=.*|ExecStart=$SCRIPT_DIR/start.sh|" "$SERVICE_FILE"
+    sed "s|ExecStart=.*|ExecStart=$SCRIPT_DIR/start.sh|" "$SERVICE_FILE_TEMPLATE" > "$SERVICE_FILE"
 
-    # Copy the updated service file to the systemd directory
-    cp "$SERVICE_FILE" /etc/systemd/system/
+    # Moove the updated service file to the systemd directory
+    mv "$SERVICE_FILE" /etc/systemd/system/
 
     # Reload the systemd daemon to apply changes
     systemctl daemon-reload
